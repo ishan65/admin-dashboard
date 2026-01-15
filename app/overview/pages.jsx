a5cbeff648aa3c2e6c9f00d3927ba9b98d9c1269
Ishan Mahajan <im530@cam.ac.uk>
2026-01-15 00:34:14 +0000

    Adding overview page

---
+++ app/overview/pages.jsx
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import StatCard from "../../components/StatCard";
+import { DollarSign, ShoppingBag, SquareActivity, Users } from "lucide-react";
+import { motion } from "framer-motion";
+
+const pages = () => {
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <main className="max-w-7xl mx-auto py-4 px-4 lg:px-8">
+        <motion.div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard name="Total Sales" icon={DollarSign} value="$123,123" />
+          <StatCard name="Total Clients" icon={Users} value="1,234" />
+          <StatCard name="Total Products" icon={ShoppingBag} value="145" />
+          <StatCard name="Stock" icon={SquareActivity} value="12,845" />
+        </motion.div>
+      </main>
+    </div>
+  );
+};
+
+export default pages;
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
+import OverviewPage from "./overview/pages";
 
 export default function Home() {
-  return <div></div>;
+  return <OverviewPage />;
 }
